2b5f91e757f405931d0bba906425f4309fc72053
Balakumaran_E <balakumaran242002@gmail.com>
2023-10-08 19:41:41 +0530

    update

--- netlify-express/index.js
+++ netlify-express/index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://balakumarandeveloper.netlify.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
